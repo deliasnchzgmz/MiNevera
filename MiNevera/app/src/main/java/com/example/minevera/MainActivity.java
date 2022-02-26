a386e9f4dc0f61218da4df081f6c96477ef521e4
deliasnchzgmz <78742129+deliasnchzgmz@users.noreply.github.com>
2022-02-26 23:05:48 +0100

    esqueletos pantallas ajustes y mapa
    +++ botones en main activity para testear

--- MiNevera/app/src/main/java/com/example/minevera/MainActivity.java
+++ MiNevera/app/src/main/java/com/example/minevera/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.minevera;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button settingsButton = (Button) findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(this::goSettings);
+        Button mapButton = (Button) findViewById(R.id.mapButton);
+        mapButton.setOnClickListener(this::goMap);
+
+    }
+
+    public void goSettings(View view) {
+        Intent settingsIntent = new Intent(this, Settings.class);
+        startActivity(settingsIntent);
+    }
+
+    public void goMap(View view) {
+        Intent mapIntent = new Intent(this, Map.class);
+        startActivity(mapIntent);
     }
 }
