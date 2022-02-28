52be267661e546e51ebc6228455ab41214cdc405
100406260 <78741438+100406260@users.noreply.github.com>
2022-02-28 21:13:36 +0100

    appbar

--- MiNevera/app/src/main/java/com/example/minevera/MainActivity.java
+++ MiNevera/app/src/main/java/com/example/minevera/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.minevera;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
@@ -13,16 +15,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button settingsButton = (Button) findViewById(R.id.settingsButton);
-        settingsButton.setOnClickListener(this::goSettings);
-        Button mapButton = (Button) findViewById(R.id.mapButton);
-        mapButton.setOnClickListener(this::goMap);
-
     }
 
-    public void goSettings(View view) {
-        Intent settingsIntent = new Intent(this, Settings.class);
-        startActivity(settingsIntent);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Se recrea el menu que aparece en AppBar de la actividad.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            System.out.println("APPMOV: Settings action...");
+            // Creamos el Intent que va a lanzar la segunda activity (SecondActivity)
+            Intent intent = new Intent(this, Settings.class);
+            // Iniciamos la nueva actividad
+            startActivity(intent);
+            return true;
+        }
+        if (id == R.id.action_about) {
+            System.out.println("APPMOV: About action...");
+
+            Intent intent = new Intent(this, AboutUs.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void goMap(View view) {
