2731563da26a410325186d8b08c551a17facd424
deliasnchzgmz <78742129+deliasnchzgmz@users.noreply.github.com>
2022-03-06 16:00:36 +0100

    commit db init

---
+++ MiNevera/app/src/main/java/com/example/minevera/AddProducts.java
@@ -0,0 +1,19 @@
+package com.example.minevera;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddProducts extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+        //TextView text_settings = (TextView)findViewById(R.id.text_add);
+
+        //settings_listview = (ListView) findViewById()
+    }
+
+
+}
---
+++ MiNevera/app/src/main/java/com/example/minevera/ProductList.java
@@ -0,0 +1,4 @@
+package com.example.minevera;
+
+public class ProductList {
+}
