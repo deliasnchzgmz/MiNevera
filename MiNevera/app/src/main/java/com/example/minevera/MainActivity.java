1862171c896501a45c34b1a3182d4e5c9d263592
paula-capel <78742238+paula-capel@users.noreply.github.com>
2022-03-08 00:11:41 +0100

    menu principal v2

--- MiNevera/app/src/main/java/com/example/minevera/MainActivity.java
+++ MiNevera/app/src/main/java/com/example/minevera/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.minevera;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +12,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //Botón de añadir a la lista de la compra
+    public void ListaCompra(android.view.View view) {
+          Snackbar.make(findViewById(R.id.mainact), R.string.b_lista_desc,
+                Snackbar.LENGTH_SHORT).show();
+    }
+    //Botón de añadir productos a la nevera
+    public void AñadirNevera(android.view.View view) {
+        Snackbar.make(findViewById(R.id.mainact), R.string.b_mas_desc,
+                Snackbar.LENGTH_SHORT).show();
+    }
+    //Botón de abrir el mapa
+    public void AbrirMapa(android.view.View view) {
+        Snackbar.make(findViewById(R.id.mainact), R.string.b_mapa_desc,
+                Snackbar.LENGTH_SHORT).show();
+    }
+
+
 }
