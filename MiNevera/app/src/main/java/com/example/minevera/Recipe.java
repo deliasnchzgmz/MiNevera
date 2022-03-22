334234532a786adaf130fac666baa5b95822e014
100406260 <78741438+100406260@users.noreply.github.com>
2022-03-22 09:18:19 +0100

    lo guarda en objetos pero no printea en pantalla

--- MiNevera/app/src/main/java/com/example/minevera/Recipe.java
+++ MiNevera/app/src/main/java/com/example/minevera/Recipe.java
@@ -15,20 +15,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,7 +33,7 @@ class EdamamRecipe {
     String ingredientLines;
     String link;
 
-    public EdamamRecipe(String name, String ingredients, String link) {
+    public EdamamRecipe() { //String name, String ingredients, String link
         this.label = "";
         this.ingredientLines = "";
         this.link = "";
@@ -75,104 +69,40 @@ class EdamamRecipe {
     }
 }
 
-public class Recipe<temp> extends AppCompatActivity {
+public class Recipe extends AppCompatActivity {
 
     // indicar API KEY para el API de tipo "browser" de Google Places
-    final String EDAMAM_KEY = "ec4ce480d955917aee29290b5a94fcd7\t";
-    final String EDAMAM_ID = "3d504fc1";
+    public String EDAMAM_KEY = "ec4ce480d955917aee29290b5a94fcd7";
+    public String EDAMAM_ID = "3d504fc1";
 
     public String q = "chicken";
     public String ingr = "3-8";
 
     private ListView m_listview;
-    String temp = makeCall("https://api.edamam.com/api/recipes/v2/json?type=public&q="
-            + q + "&app_id=" + EDAMAM_ID + "&app_key=" + EDAMAM_KEY + "&ingr=" + ingr);;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
-    }
 
-    //CLASE PARA LEER FICHERO JSON VIDEO DEL SEÑOR
-    /*public void readJson(View v){
-        //String json_string = "{'label':'patatas fritas','ingredientLines':'patatas\nsal','link':'link'}";
-        //Gson gson = new Gson();
+        m_listview = (ListView) findViewById(R.id.id_list_view);
 
-        //EdamamRecipe receta = gson.fromJson(json_string, EdamamRecipe.class);
-
-        //Log.d("prueba", receta.toString());
-        try {
-            InputStream inputstream = getAssets().open("recetas.json");
-            int size = inputstream.available();
-            byte[] buffer = new byte[size];
-            inputstream.read(buffer);
-            inputstream.close();
-
-            String json_string = new String(buffer, "UTF-8");
-
-
-
-             Gson gson = new Gson();
-
-            EdamamRecipe receta = gson.fromJson(json_string, EdamamRecipe.class);
-
-            Log.d("prueba", receta.toString());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
-    public String makeCall(String stringURL) { //
-
-        URL url = null;
-        BufferedInputStream is = null;
-        Gson gson = new Gson();
-        ArrayList<EdamamRecipe> temp = new ArrayList<EdamamRecipe>();
-        String str ;
-
-        try {
-            url = new URL(stringURL);
-        } catch (Exception ex) {
-            System.out.println("Malformed URL");
-        }
-
-        try {
-            if (url != null) {
-                URLConnection urlConnection = url.openConnection();
-                urlConnection.connect();
-                BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                StringBuilder sb = new StringBuilder();
-
-                while((str = br.readLine()) != null){
-                    sb.append(str + "\n");
-                }
-                br.close();
-                Log.d("prueba", sb.toString());
-                return sb.toString();
-
-                //is = new BufferedInputStream(urlConnection.getInputStream());
-            }
-        } catch (IOException ioe) {
-            System.out.println("IOException");
-        }
-        return null;
+        new EdamamRecipes().execute();
     }
 
 
-    /*public class EdamamRecipes extends AsyncTask<View, Void, ArrayList<EdamamRecipe>> {
+    public class EdamamRecipes extends AsyncTask<View, Void, ArrayList<EdamamRecipe>> {
 
         @Override
         protected ArrayList<EdamamRecipe> doInBackground(View... urls) {
             ArrayList<EdamamRecipe> temp;
             //print the call in the console
-            System.out.println("https://api.edamam.com/api/recipes/v2/json?type=public&q="
-                    + q + "&app_id=" + EDAMAM_ID + "&app_key=" + EDAMAM_KEY + "&ingr=" + ingr);
+            System.out.println("https://api.edamam.com/api/recipes/v2?type=public&q="
+                    + q + "&app_id=" + EDAMAM_ID + "&app_key=" + EDAMAM_KEY + "&ingr=3-8");
 
             // make Call to the url
-            temp = makeCall("https://api.edamam.com/api/recipes/v2/json?type=public&q="
-                    + q + "&app_id=" + EDAMAM_ID + "&app_key=" + EDAMAM_KEY + "&ingr=" + ingr);
-
+            //temp = makeCall("https://api.edamam.com/api/recipes/v2?type=public&q=" + q + "&app_id=" + EDAMAM_ID + "&app_key=" + EDAMAM_KEY + "&ingr=3-8");
+            temp = makeCall("https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=3d504fc1&app_key=ec4ce480d955917aee29290b5a94fcd7&ingr=3-8&field=label");
             return temp;
         }
 
@@ -189,7 +119,7 @@ public class Recipe<temp> extends AppCompatActivity {
             for (int i = 0; i < result.size(); i++) {
                 // make a list of the venus that are loaded in the list.
                 // show the name, the category and the city
-                listTitle.add(i, result.get(i).toString());
+                listTitle.add(i, result.get(i).getName());
             }
 
             // set the results to the list
@@ -225,20 +155,47 @@ public class Recipe<temp> extends AppCompatActivity {
 
         if (is != null) {
             try {
-                JsonParser jp = new JsonParser();
-                JsonElement root = jp.parse(new InputStreamReader((InputStream) is ));
-                JsonObject rootobj = root.getAsJsonObject();
-                String rootstr = new String(rootobj.getAsString());
-                Log.d("prueba", rootstr);
-
-
+                JsonReader jsonReader = new JsonReader(new InputStreamReader(is, "UTF-8"));
+                jsonReader.beginObject();
+                while (jsonReader.hasNext()){
+                    String name = jsonReader.nextName();
+                    if (name.equals("hits")){
+                        jsonReader.beginArray();
+                        while (jsonReader.hasNext()){
+                            EdamamRecipe receta = new EdamamRecipe();
+                            jsonReader.beginObject();
+                            while (jsonReader.hasNext()){
+                                name = jsonReader.nextName();
+                                if (name.equals("recipe")){
+                                    jsonReader.beginObject();
+                                    while (jsonReader.hasNext()){
+                                        name = jsonReader.nextName();
+                                        if (name.equals("label")){
+                                            receta.setLabel(jsonReader.nextString());
+                                            System.out.println("Receta: " + receta.getName());
+                                        } else {
+                                            jsonReader.skipValue();
+                                        }
+                                    }
+                                    jsonReader.endObject();
+                                } else {
+                                    jsonReader.skipValue();
+                                }
+                            }
+                            jsonReader.endObject();
+                            temp.add(receta);
+                        }
+                        jsonReader.endArray();
+                    } else {
+                        jsonReader.skipValue();
+                    }
+                }
             } catch (Exception e) {
                 System.out.println("Exception");
                 return new ArrayList<EdamamRecipe>();
             }
         }
-
         return temp;
-    }*/
+    }
 
 }
