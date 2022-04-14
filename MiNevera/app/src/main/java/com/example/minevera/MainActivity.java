fb5536e04f4c828d3913b798a70cc943c3ca0408
100406260 <78741438+100406260@users.noreply.github.com>
2022-04-14 22:22:19 +0200

    botones flotantes sin colocar bien :(

--- MiNevera/app/src/main/java/com/example/minevera/MainActivity.java
+++ MiNevera/app/src/main/java/com/example/minevera/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.database.Cursor;
 import android.widget.ListView;
@@ -67,8 +70,8 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    //Botón de añadir a la lista de la compra
-    public void ListaCompra(android.view.View view) {
+    //Botón de Recetas
+    public void Recetas(android.view.View view) {
           Snackbar.make(findViewById(R.id.mainact), R.string.b_lista_desc,
                 Snackbar.LENGTH_SHORT).show();
           Intent listIntent = new Intent(this, SearchIngr.class);
@@ -76,12 +79,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //Botón de añadir productos a la nevera
-    public void AñadirNevera(android.view.View view) {
-        Snackbar.make(findViewById(R.id.mainact), R.string.b_mas_desc,
-                Snackbar.LENGTH_SHORT).show();
-        Intent addIntent = new Intent(this, AddMenu.class);
+    public void AñadirNevera(android.view.View view){
+        ImageButton boton_camara =  findViewById(R.id.boton_camara);
+        ImageButton boton_bbdd = findViewById(R.id.boton_bbdd);
+
+        boton_camara.setVisibility(View.VISIBLE);
+        boton_bbdd.setVisibility(View.VISIBLE);
+    }
+
+    public void AbrirCamara(View view) {
+        Intent addIntent = new Intent(this, Camara.class);
         startActivity(addIntent);
+        finish();
+    }
+
+    //Botón de añadir a la lista de la compra manualmente
+    public void AñadirManualmente(View view) {
+        Intent listIntent = new Intent(this, AddProducts.class);
+        startActivity(listIntent);
+        finish();
     }
 
     //Botón de abrir el mapa
--- MiNevera/app/src/main/java/com/example/minevera/Map.java
+++ MiNevera/app/src/main/java/com/example/minevera/Map.java
@@ -1,45 +1,47 @@
 package com.example.minevera;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Map extends AppCompatActivity{
+import java.util.List;
+
+
+// Basado en código de Alicia Rodríguez Carrión
+
+// Implementamos la interfaz LocationListener, del framework de localización
+// propio de Android: android.location
+
+// NOTA: Desde Android nos recomiendan encarecidamente que utilicemos la API
+// de Google Play en lugar de este framework, por su simplicidad y eficiencia.
+// Nosotros encuentramos más sencillo utilizar el framework nativo y
+// el framework sigue estando soportado, por lo que hemos hecho el ejemplo usando
+// este y no la API de Google Play.
+
+public class Map extends FragmentActivity implements OnMapReadyCallback {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-        TextView text_settings = (TextView)findViewById(R.id.text_map);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onMapReady(@NonNull GoogleMap googleMap) {
 
-        if (id == R.id.action_settings) {
-            System.out.println("APPMOV: About settings...");
-
-            Intent intent = new Intent(this, Settings.class);
-            startActivity(intent);
-            return true;
-        }
-        if (id == R.id.action_about) {
-            System.out.println("APPMOV: About action...");
-
-            Intent intent = new Intent(this, AboutUs.class);
-            startActivity(intent);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
